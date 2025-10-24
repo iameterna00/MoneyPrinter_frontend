47ac61dc6f8f51f26ab99699f40738ee657371bc
iameterna00 <12345678+iameterna00@users.noreply.github.com>
2025-10-24 13:33:32 +0545

    ad

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 
 export const webApi = window.location.hostname === "localhost"
   ? 'http://127.0.0.1:8000/api' 
-  : 'https://nepwoopbackend-1.onrender.com/api' 
+  : 'https://geography-vice-son-cement.trycloudflare.com/api' 
