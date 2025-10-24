571ed66e40fdaadd063ceb5cbf62b96ffdac2e2b
iameterna00 <12345678+iameterna00@users.noreply.github.com>
2025-10-24 17:03:01 +0545

    cors

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 
 export const webApi = window.location.hostname === "localhost"
   ? 'http://127.0.0.1:8000/api' 
-  : 'https://reef-tan-wesley-peer.trycloudflare.com ' 
+  : 'https://reef-tan-wesley-peer.trycloudflare.com/api' 
