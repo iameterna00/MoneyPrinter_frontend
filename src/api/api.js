11106b4fb824d7dd0867beda75cee1bb37742bae
iameterna00 <iameterna00@users.noreply.github.com>
2025-10-27 14:12:35 +0545

    asd

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 
 export const webApi = window.location.hostname === "localhost"
   ? 'http://127.0.0.1:8000/api' 
-  : 'https://reef-tan-wesley-peer.trycloudflare.com/api' 
+  : 'https://packet-collectables-menus-teaching.trycloudflare.com/api'; 
