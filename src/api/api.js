93484dad504e7a309b49ad2c863b58051b8a60fc
iameterna00 <iameterna00@users.noreply.github.com>
2025-10-29 12:54:44 +0545

    ad

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 
 export const webApi = window.location.hostname === "localhost"
   ? 'http://127.0.0.1:8000/api' 
-  : 'https://viking-visits-constitutes-handhelds.trycloudflare.com/api'; 
+  : 'https://disco-duck-calculators-criticism.trycloudflare.com/api'; 
