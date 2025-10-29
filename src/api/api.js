b88b65577468a6397616eacf7e1a848407869b43
iameterna00 <iameterna00@users.noreply.github.com>
2025-10-30 00:31:52 +0545

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 
 export const webApi = window.location.hostname === "localhost"
   ? 'http://127.0.0.1:8000/api' 
-  : 'https://disco-duck-calculators-criticism.trycloudflare.com/api'; 
+  : 'https://junior-exclusively-county-asked.trycloudflare.com /api'; 
