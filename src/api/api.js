38f5b8b59b81100fed54160820607d989af04fb3
iameterna00 <iameterna00@users.noreply.github.com>
2025-10-30 00:33:20 +0545

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 
 export const webApi = window.location.hostname === "localhost"
   ? 'http://127.0.0.1:8000/api' 
-  : 'https://junior-exclusively-county-asked.trycloudflare.com /api'; 
+  : 'https://junior-exclusively-county-asked.trycloudflare.com/api'; 
