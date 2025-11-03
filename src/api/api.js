ab1f8308575d518eab8faae4a9bafb1cd04c923d
iameterna00 <iameterna00@users.noreply.github.com>
2025-11-03 14:15:28 +0545

    ad

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
 
 export const webApi = window.location.hostname === "localhost"
   ? 'http://127.0.0.1:8000/api' 
-  : 'https://bunny-plastics-math-und.trycloudflare.com/api'; 
+  : 'https://put-thermal-unfortunately-representatives.trycloudflare.com/api'; 
